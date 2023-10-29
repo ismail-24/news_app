989ef924cd9ca398b9b904f00bb14b2573f61c42
ismail-24 <ismailtarek888@gmail.com>
2023-10-29 09:43:42 +0200

    Update category_view.dart

--- lib/views/category_view.dart
+++ lib/views/category_view.dart
@@ -8,12 +8,34 @@ class CategoryView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          NewsListViewBuilder(
-            category: category,
-          ),
-        ],
+      appBar: AppBar(
+        foregroundColor: Colors.orange,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'News',
+              style: TextStyle(color: Colors.black),
+            ),
+            Text(
+              'Cloud',
+              style: TextStyle(color: Colors.orange),
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: CustomScrollView(
+          slivers: [
+            NewsListViewBuilder(
+              category: category,
+            ),
+          ],
+        ),
       ),
     );
   }
